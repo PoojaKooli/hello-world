8a276beb161c38b3dd24ebf3e22b75e5bae8862c
Pooja <Pooja@blrkec135231l.ad.infosys.com>
2016-09-19 16:41:25 +0530

    Added a sample Xcode project

---
+++ SampleProject/SampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Pooja on 9/19/16.
+//  Copyright © 2016 Infosys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
