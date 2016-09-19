3830c7d1838e545911f8ea9515fc4f3a32b30587
poojakooli <pooja.spanj@gmail.com>
2016-09-19 17:24:17 +0530

    commit 2

--- SampleProject/SampleProject/AppDelegate.swift
+++ SampleProject/SampleProject/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		// Override point for customization after application launch.
 		// test changes in branch 2
 		//commit 1
+		//commit 2
 		return true
 	}
 
